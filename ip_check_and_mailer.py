00ff9f04666a6916bd7584e1b645ac2b7e811f06
cemonatk <cemkaragun@gmail.com>
2015-10-18 01:24:13 +0300

    Update ip_check_and_mailer.py

--- ip_check_and_mailer.py
+++ ip_check_and_mailer.py
@@ -7,9 +7,7 @@ def ipcheck():
 	server = smtplib.SMTP("smtp.gmail.com", 587)
 	server.starttls()
 	server.login(" yourmail  @gmail.com","password")
-	addr = urllib.URLopener()
-	response = addr.open("http://echoip.com/")
-	ip = response.readline()
+	ip = urllib.URLopener().open("http://echoip.com/").readline()
 	if ip in open('ip.txt','a+').read():
 		mail = "IP adress still the same and %s " % ip
 	else:
