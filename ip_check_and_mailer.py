d44db3d8495ba29be48fdf17167bbc435cbdddb3
cemonatk <cemkaragun@gmail.com>
2015-10-18 01:08:21 +0300

    Update ip_check_and_mailer.py

--- ip_check_and_mailer.py
+++ ip_check_and_mailer.py
@@ -4,13 +4,12 @@ import smtplib,urllib,time
 from email.mime.text import MIMEText
 
 def ipcheck():
-	server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-	server.login("   @gmail.com","password")
-	
+	server = smtplib.SMTP("smtp.gmail.com", 587)
+	server.starttls()
+	server.login(" yourmail  @gmail.com","password")
 	addr = urllib.URLopener()
 	response = addr.open("http://echoip.com/")
 	ip = response.readline()
-	
 	if ip in open('ip.txt','a+').read():
 		mail = "IP adress still the same and %s " % ip
 	else:
@@ -18,12 +17,11 @@ def ipcheck():
 		f = open('ip.txt','w')
 		f.write(ip)
 		f.close()
-		
 	msg = MIMEText(mail)
 	msg["Subject"] = "Server IP Adress"
-	msg["From"] = "   @gmail.com"
-	msg["To"] = "   @gmail.com"
-	server.sendmail("   @gmail.com", ["   @gmail.com"], msg.as_string())
+	msg["From"] = " yourmail  @gmail.com"
+	msg["To"] = " Sendto  @gmail.com"
+	server.sendmail(" yourmail  @gmail.com", [" Sendto  @gmail.com"], msg.as_string())
 	server.quit()
 	
 while True:
