91d107bc325351cc3083821f9449f37b56fb2c00
cemonatk <cemkaragun@gmail.com>
2015-10-21 11:00:04 +0300

    Update ip_check_and_mailer.py

--- ip_check_and_mailer.py
+++ ip_check_and_mailer.py
@@ -6,23 +6,28 @@ __AUTHOR__ = 'cemonatk'
 import smtplib,urllib,time
 from email.mime.text import MIMEText
 
+receiver    = "xxxx@gmail.com"
+transmitter = "yourmail@gmail.com"
+password    = "yourgmailpassword"
+
 def ipcheck():
 	server = smtplib.SMTP("smtp.gmail.com", 587)
 	server.starttls()
-	server.login(" yourmail  @gmail.com","password")
+	server.login(transmitter,password)
 	ip = urllib.URLopener().open("http://echoip.com/").readline()
 	if ip in open('ip.txt','a+').read():
-		mail = "IP adress still the same and %s " % ip
+		#mail = "IP adress still the same and %s " % ip
+		pass
 	else:
 		mail = "New IP adress is %s " % ip
 		open('ip.txt','w').write(ip)
 	msg = MIMEText(mail)
 	msg["Subject"] = "Server IP Adress"
-	msg["From"] = " yourmail  @gmail.com"
-	msg["To"] = " Sendto  @gmail.com"
-	server.sendmail(" yourmail  @gmail.com", [" Sendto  @gmail.com"], msg.as_string())
+	msg["From"] = transmitter
+	msg["To"] = receiver
+	server.sendmail(transmitter, [receiver], msg.as_string())
 	server.quit()
 	
 while True:
 	ipcheck()
-	time.sleep(7200)
+	time.sleep(1800)
